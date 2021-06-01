51988b18dbbeb4fc8dee9a6119012d6b9f6363be
Debajyoti_Roy_Chowdhury <63442163+debajyotiroyc@users.noreply.github.com>
2021-06-01 20:13:43 +0530

    Add files via upload

---
+++ RadioButton.js
@@ -0,0 +1,12 @@
+function f1()
+            {
+                var a=document.getElementById("op1");
+                var b=document.getElementById("op2");
+                var c=document.getElementById("op3");
+                var d=document.getElementById("op4");
+
+                if(a.checked==true)
+                alert("Selected="+a.value+" "+a.type);
+                else
+                alert("value= "+b.name);
+            }
