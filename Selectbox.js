1211cfcd4666d8e9861e47fa58f5ea5c1886eca9
Debajyoti_Roy_Chowdhury <63442163+debajyotiroyc@users.noreply.github.com>
2021-06-04 19:24:34 +0530

    Add files via upload

---
+++ Selectbox.js
@@ -0,0 +1,7 @@
+function f1()
+            {
+                var x=document.getElementById("selectbox");
+                alert(x.options[x.selectedIndex].value);
+                var a=document.getElementById("ok");
+                alert(a.innerHTML);
+            }
