f7346d0a29e02a6c656fa2f909fbf816c61d54b7
Debajyoti_Roy_Chowdhury <63442163+debajyotiroyc@users.noreply.github.com>
2021-06-19 21:16:43 +0530

    Add files via upload

---
+++ MultipleTags.js
@@ -0,0 +1,9 @@
+<script>
+            function stylechange()
+            {
+               var x =document.getElementsByTagName("p");
+               x[0].style.fontSize=30;
+               x[1].style.color="blue";
+               alert(x[3]);
+            }
+        </script>
